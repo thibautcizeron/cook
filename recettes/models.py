80f076a562445550b2c522fb4e285fb4475c750c
Thibaut CIZERON <thib.czr2002@gmail.com>
2025-05-26 07:32:39 +0200

    change title on loading page

--- recettes/models.py
+++ recettes/models.py
@@ -2,6 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+class Image(models.Model):
+    """Modèle centralisé pour gérer toutes les images"""
+    nom = models.CharField(max_length=255)
+    fichier = models.ImageField(upload_to='images/')
+    description = models.TextField(blank=True, null=True)
+    date_creation = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.nom
+    
+    class Meta:
+        verbose_name = "Image"
+        verbose_name_plural = "Images"
+
 class Categorie(models.Model):
     nom = models.CharField(max_length=255, unique=True)
     image = models.ImageField(upload_to='categories/', null=True, blank=True)  
